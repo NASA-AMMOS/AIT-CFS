7f7890b499c324ea3a32c0c867e5d58f5574382c
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2017-11-01 17:57:39 -0700

    Add bliss-gui to dependencies

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ setup(
     author_email = 'bliss@jpl.nasa.gov',
 
     namespace_packages = ['bliss'],
-    install_requires   = ['bliss-core>=0.26.1'],
+    install_requires   = [
+        'bliss-core>=0.26.1',
+        'bliss-gui>=0.13.2',
+    ],
     dependency_links = [
-       'https://bliss.jpl.nasa.gov/pypi/simple/bliss-core/'
+       'https://bliss.jpl.nasa.gov/pypi/simple/bliss-core/',
+       'https://bliss.jpl.nasa.gov/pypi/simple/bliss-gui/'
     ],
 )
